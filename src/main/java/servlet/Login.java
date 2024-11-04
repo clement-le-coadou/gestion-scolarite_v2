6442d7933361716abc45b0a2dab89c3436441cbc
bapger <153543264+bapger@users.noreply.github.com>
2024-11-04 14:32:22 +0100

    delete enseignant,etudiant ect

--- src/main/java/servlet/Login.java
+++ src/main/java/servlet/Login.java
@@ -38,6 +38,13 @@ public class Login extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String user_type=request.getParameter("user_type");
 		String username = request.getParameter("username");
 	    String password = request.getParameter("password");
@@ -106,14 +113,6 @@ public class Login extends HttpServlet {
 	    }
 	    
         response.sendRedirect("accueil.jsp");
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
 	}
 
 }
---
+++ src/main/java/servlet/SupprimerEtudiant.java
@@ -0,0 +1,59 @@
+package servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jpa.Cours;
+import jpa.Etudiant;
+
+import java.io.IOException;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import dao.EtudiantDAO;
+import daogenerique.CrudGeneric;
+import daogenerique.GenericDAO;
+
+/**
+ * Servlet implementation class SupprimerEtudiant
+ */
+@WebServlet("/SupprimerEtudiant")
+public class SupprimerEtudiant extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+	private CrudGeneric<Etudiant> etudiantDAO = new CrudGeneric<>(sessionFactory, Etudiant.class);
+    
+	/**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SupprimerEtudiant() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Long id = Long.parseLong(request.getParameter("id"));
+        
+        Etudiant etudiant = etudiantDAO.read(id);
+        if (etudiant != null) {
+        	etudiantDAO.delete(etudiant);
+        }
+
+        response.sendRedirect("GestionEtudiants.jsp");
+	}
+
+}
